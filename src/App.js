692cad67fe01626d7d4ba0d99447e0a5af7fd84a
Manogya Acharya <acharyma@bc.edu>
2020-12-22 16:26:20 -0500

    eslint script ran; still some errors left to resolve

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import React, {useState} from 'react';
 import Tweet from './Tweet'
 import './App.css';
 
 function App() {
-    const [users, setUser] = useState([
+    const [users] = useState([
         {name: "Manogya", tweet: "That's the whole tweet"},
         {name: "Suyogya", tweet: "Word"},
         {name: "Someone", tweet: "The show must go on"}
